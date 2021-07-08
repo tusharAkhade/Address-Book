c7badbf789ecd6afc00bb5a45bdd6d9d12d8c443
Tushar Akhade <tusharakhade10@gmail.com>
2021-07-08 23:48:35 +0530

    Added search Searching a person in a city or state across the multiple address book

--- src/service/AddressBookServiceInterface.java
+++ src/service/AddressBookServiceInterface.java
@@ -9,4 +9,6 @@ public interface AddressBookServiceInterface {
     public abstract void addAddressBook(HashMap<String, ArrayList<Person>> addressBook, String companyName, ArrayList<Person> personArrayList);
     public abstract void editAddressBook(HashMap<String, ArrayList<Person>> addressBook, String companyName, ArrayList<Person> personArrayList);
     public abstract void viewAddressBook(HashMap<String, ArrayList<Person>> addressBook);
-}
+    public abstract void searchPersonByCityOrState(HashMap<String, ArrayList<Person>> addressBook);
+
+    }
